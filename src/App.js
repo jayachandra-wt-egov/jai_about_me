b7b5fcc53e610145a22deaa64755130f587a19e5
jayachandra-wt-egov <jayachandra.choppa@egovernments.org>
2020-10-26 11:21:14 +0530

    prasad changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save Ritika to reload.
+          Edit <code>src/App.js</code> and save Prasad to reload.
         </p>
         <p>
 Hello Testyer        </p>
